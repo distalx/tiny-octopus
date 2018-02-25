31d33d7a70aae817dfbb2f240a703c638cf6921a
distalx <distalx@gmail.com>
2018-02-25 08:42:10 +0530

    test(QueryBuilder): missing tests

--- src/query_builder.test.js
+++ src/query_builder.test.js
@@ -1,1 +1,27 @@
+const expect = require ('chai').expect;
 const QueryBuilder = require('./query_builder');
+
+describe('QueryBuilder',function(){
+  it('update query',function(){
+    const task = {
+      source_c: "tags",
+      destination_c: ['posts'],
+      source_f: "_id",
+      destination_f: "tags.id",
+      fields: {
+        'title': "tags.$.title"
+      }
+    };
+
+    const change = {
+      documentKey: { _id: '2' },
+      updateDescription: { updatedFields: { title: 'JS' }, removedFields: [] }
+    };
+
+    const { filter, update } = QueryBuilder(task, change)
+
+    expect(filter).to.deep.equal({ 'tags.id': '2' });
+    expect(update).to.deep.equal({ '$set': { 'tags.$.title': 'JS' } });
+
+  });
+});
